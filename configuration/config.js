b64368c8e4c9c4238a1783ca097b10fc5e87204c
TruongLX <truonglx@bkav.com>
2017-10-26 16:33:55 +0700

    truonglx

---
+++ configuration/config.js
@@ -0,0 +1,15 @@
+module.exports={
+/*
+* This file contains the configurations information of Twitter login app.
+* It consists of Twitter app information, database information.
+*/
+
+	"facebook_api_key" 		: 			"1104805722988539",
+	"facebook_api_secret"	:				"7f877c60b3938c9b66556d020c8721cb",
+	"callback_url"				:				"http://localhost:3000/auth/facebook/callback",
+	"use_database"				:				"false",
+	"host"								:				"localhost",
+	"username"						:				"root",
+	"password"						:				"",
+	"database"						:				""
+}
